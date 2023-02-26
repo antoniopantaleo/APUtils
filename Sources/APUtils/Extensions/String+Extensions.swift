6b8bdd1c47a6b6aa30ab7386f4b49a79df4a9708
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2023-02-26 01:53:03 +0100

    [String+Extensions] add utils + tests

---
+++ Sources/APUtils/Extensions/String+Extensions.swift
@@ -0,0 +1,31 @@
+//
+//  String+Extensions.swift
+//  
+//
+//  Created by Antonio on 26/02/23.
+//
+
+import Foundation
+
+public extension String {
+    subscript(_ range: CountableRange<Int>) -> String {
+        let start = index(startIndex, offsetBy: max(0, range.lowerBound))
+        let end = index(start, offsetBy: min(self.count - range.lowerBound, range.upperBound - range.lowerBound))
+        return String(self[start..<end])
+    }
+    
+    subscript(_ range: CountablePartialRangeFrom<Int>) -> String {
+        let start = index(startIndex, offsetBy: max(0, range.lowerBound))
+        return String(self[start...])
+    }
+    
+    static func loremIpsum(ofLength length: Int = 445) -> String {
+        guard length > 0 else { return "" }
+        let loremIpsum: String = """
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        """
+        return loremIpsum.prefix(length).description
+    }
+    
+    static var loremIpsum: String { loremIpsum() }
+}
