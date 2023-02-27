fb57294eeb9411b995c2cef7f00c6f6f9ba93b64
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2023-02-27 22:54:00 +0100

    indent package.swift

--- Package.swift
+++ Package.swift
@@ -7,23 +7,19 @@ let package = Package(
     name: "APUtils",
     platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "APUtils",
             targets: ["APUtils"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "APUtils",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "APUtilsTests",
-            dependencies: ["APUtils"]),
+            dependencies: ["APUtils"]
+        )
     ]
 )
