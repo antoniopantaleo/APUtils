3eef8571c6b1bac6dcf1d7195ee7351a8663e743
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2023-03-28 00:54:40 +0200

    [LRUCache] add documentation comments

--- Sources/APUtils/Models/LRUCache.swift
+++ Sources/APUtils/Models/LRUCache.swift
@@ -7,11 +7,33 @@
 
 import Foundation
 
+/// An implementation of a Least Recently Used (LRU) Cache data structure
+///
+/// The cache keeps in memory last used items.
+/// The oldest get removed when the number of items exceeds the capacity of the cache
+///
+/// ```swift
+/// cache["zero"] = 0
+/// cache["one"] = 1
+/// cache["two"] = 2
+/// cache["three"] = 3
+/// cache["four"] = 4
+///
+/// print(cache["one"])     // 1
+/// print(cache["two"])     // 2
+/// print(cache["three"])   // 3
+/// print(cache["four"])    // 4
+/// print(cache["zero"])    // nil
+/// ```
 public class LRUCache<Key: Hashable, Value> {
     private var cache: [Key:Value] = [:]
     private var recentlyUsedKeys: [Key] = []
     private let maxCapacity: Int
     
+    /// Creates a new cache
+    /// - Parameter maxCapacity: The max number of items to keep in memory.
+    ///
+    /// Oldest unused items will be removed from the cache
     public init(maxCapacity: Int = 20) {
         self.maxCapacity = maxCapacity
     }
