4d826e72718513e16b6a7d0ee5e9c4906701a71a
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2023-06-22 00:05:49 +0200

    [Thread] add mainThread function

---
+++ Tests/APUtilsTests/ThreadTests.swift
@@ -0,0 +1,29 @@
+//
+//  ThreadTests.swift
+//  
+//
+//  Created by Antonio on 21/06/23.
+//
+
+import XCTest
+import APUtils
+
+final class ThreadTests: XCTestCase {
+    
+    func testThread_isMainThread() {
+        // Given
+        let expectation = XCTestExpectation(description: "Code runs on main thread")
+        let backgroundThread = DispatchQueue(label: "background", qos: .background)
+        // When
+        backgroundThread.async {
+            XCTAssertFalse(Thread.isMainThread)
+            mainThread {
+                // Then
+                XCTAssertTrue(Thread.isMainThread)
+                expectation.fulfill()
+            }
+        }
+        wait(for: [expectation])
+    }
+    
+}
