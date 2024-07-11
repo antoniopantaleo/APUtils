e7ef1489fe29a0e8e61c2600bc6dd79f3b199e90
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2024-07-11 16:27:19 +0200

    feat(attributed-string-builder)!: replace NSAttributedString with AttributedString
    This will increase minimum iOS/MacOS target to 15/13

--- Tests/APUtilsTests/NSAttributedStringBuilderTests.swift
+++ Tests/APUtilsTests/NSAttributedStringBuilderTests.swift
@@ -8,62 +8,22 @@
 import XCTest
 import APUtils
 
+@available(iOS 15, macOS 12, *)
 final class NSAttributedStringBuilderTests: XCTestCase {
 
     func test_onlyStringsCreatesSimpleStringWithNoAttributes() {
-        let sut = NSAttributedString {
+        let sut = AttributedString {
             "Hello"
             "World"
         }
         XCTAssertEqual(sut.string, "Hello World")
-        (0..<sut.length).forEach {
-            XCTAssertTrue(sut.attributes(at: $0, effectiveRange: nil).isEmpty)
-        }
     }
-    
-    func test_bold() {
-        let sut = NSAttributedString {
-            "Hello"
-            Bold("World")
-        }
-        XCTAssertEqual(sut.string, "Hello World")
-        (0..<5).forEach {
-            XCTAssertTrue(sut.attributes(at: $0, effectiveRange: nil).isEmpty)
-        }
-        (6..<sut.length).forEach {
-            let attributes = sut.attributes(at: $0, effectiveRange: nil)
-            XCTAssertEqual(attributes.count, 1)
-            #if canImport(UIKit)
-            XCTAssertEqual(attributes[.font] as? NSFont, UIFont.boldSystemFont(ofSize: 17))
-            #elseif canImport(AppKit)
-            XCTAssertEqual(attributes[.font] as? NSFont, NSFont.boldSystemFont(ofSize: 17))
-            #else
-            XCTFail("No platform")
-            #endif
-        }
-    }
-    
-    func test_bold_closure() {
-        let sut = NSAttributedString {
-            "Hello"
-            Bold {
-                "World"
-            }
-        }
-        XCTAssertEqual(sut.string, "Hello World")
-        (0..<5).forEach {
-            XCTAssertTrue(sut.attributes(at: $0, effectiveRange: nil).isEmpty)
-        }
-        (6..<sut.length).forEach {
-            let attributes = sut.attributes(at: $0, effectiveRange: nil)
-            XCTAssertEqual(attributes.count, 1)
-            #if canImport(UIKit)
-            XCTAssertEqual(attributes[.font] as? NSFont, UIFont.boldSystemFont(ofSize: 17))
-            #elseif canImport(AppKit)
-            XCTAssertEqual(attributes[.font] as? NSFont, NSFont.boldSystemFont(ofSize: 17))
-            #else
-            XCTFail("No platform")
-            #endif
-        }
+
+}
+
+
+fileprivate extension AttributedString {
+    var string: String {
+        NSAttributedString(self).string
     }
 }
