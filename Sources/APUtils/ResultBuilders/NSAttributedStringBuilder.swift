66f246bdd608263ffd1a2aa80363ceb34b554f14
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2024-07-11 16:27:19 +0200

    feat(attributed-string-builder)!: replace NSAttributedString with AttributedString
    This will increase minimum iOS/MacOS target to 15/13

--- Sources/APUtils/ResultBuilders/NSAttributedStringBuilder.swift
+++ Sources/APUtils/ResultBuilders/NSAttributedStringBuilder.swift
@@ -6,87 +6,37 @@
 //
 
 import Foundation
-#if canImport(UIKit)
-import UIKit
-#elseif canImport(AppKit)
-import AppKit
-#endif
-
-public protocol ExpressibleByNSAttribute {
-    var value: String { get }
-    var attributes: [NSAttributedString.Key : Any]? { get }
-}
-
-public struct Bold: ExpressibleByNSAttribute {
-    public var attributes: [NSAttributedString.Key : Any]? {
-        #if canImport(UIKit)
-        [.font: UIFont.boldSystemFont(ofSize: 17)]
-        #elseif canImport(AppKit)
-        [.font: NSFont.boldSystemFont(ofSize: 17)]
-        #else
-        nil
-        #endif
-    }
-    
-    public let value: String
-    
-    public init(_ value: String) {
-        self.value = value
-    }
-    
-    public init(_ value: () -> String) {
-        self.value = value()
-    }
-}
 
 // MARK: - Result Builder
 @resultBuilder
-public enum NSAttributedStringBuilder {
+@available(iOS 15, macOS 12, *)
+public enum AttributedStringBuilder {
     
-    public static func buildBlock(_ components: NSAttributedString...) -> NSAttributedString {
-        let string = NSMutableAttributedString()
-        components.forEach {
-            let fullRange = NSRange(location: 0, length: $0.length)
-            var currentRange = NSRange(location: 0, length: 0)
-            let attributes = $0.attributes(at: 0, longestEffectiveRange: &currentRange, in: fullRange)
-            string.append(
-                NSAttributedString(
-                    string: $0.string + " ",
-                    attributes: attributes
-                )
-            )
-        }
-        string.deleteCharacters(
-            in: NSRange(
-                location: string.length - 1,
-                length: 1
-            )
-        )
-        return string
+    public static func buildBlock(_ components: AttributedString...) -> AttributedString {
+        guard let last = components.last else { return AttributedString() }
+        let separator = AttributedString(" ")
+        return components.dropLast().reduce(AttributedString()) { partialResult, component in
+            partialResult + component + separator
+        } + last
     }
 }
 
 // MARK: - Expressions
-extension NSAttributedStringBuilder {
-    public static func buildExpression(_ expression: NSAttributedString) -> NSAttributedString {
-        expression
+@available(iOS 15, macOS 12, *)
+extension AttributedStringBuilder {
+    public static func buildExpression(_ expression: AttributedString) -> AttributedString {
+        return expression
     }
 
-    public static func buildExpression(_ expression: String) -> NSAttributedString {
-        NSAttributedString(string: expression)
-    }
-    
-    public static func buildExpression<T: ExpressibleByNSAttribute>(_ expression: T) -> NSAttributedString {
-        return NSAttributedString(
-            string: expression.value,
-            attributes: expression.attributes
-        )
+    public static func buildExpression(_ expression: String) -> AttributedString {
+        return AttributedString(expression)
     }
 }
 
-// MARK: - NSAttributedString convenience init
-public extension NSAttributedString {
-    convenience init(@NSAttributedStringBuilder _ builder: () -> NSAttributedString) {
-        self.init(attributedString: builder())
+// MARK: - AttributedString convenience init
+@available(iOS 15, macOS 12, *)
+public extension AttributedString {
+    init(@AttributedStringBuilder _ builder: () -> AttributedString) {
+        self = builder()
     }
 }
